7c204fe0becc87f20596181400ac7aad0bed1c76
KristienN <62649348+KristienN@users.noreply.github.com>
2021-07-25 16:23:53 +0200

    styling to prediction pages

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,36 +5,21 @@ $(document).ready(function () {
         url: "https://qtscore.herokuapp.com/prediction/",
         success: function (response) {
             response.forEach(res => {
-                $card = `
-                                <div class="row p-5 align-items-center justify-content-center text-center mb-3 text-light" style="border: 2px solid black; background-color: black;">
-                                <div class="col-md-2 d-flex flex-column align-items-center justify-content-center">
-                                    <p><strong>Country</strong></p>
-                                    <img src="${res.country}" class="img-fluid">
-                                </div>
-                                <div class="col-md-2">
-                                    <h4 class="fe_pred">${res.home_team}</h4>
-                                </div>
-                                <div class="col-md-1 align-items-center justify-content-center"><h4>vs</h4></div>
-                                <div class="col-md-2">
-                                    <h4 class="fe_pred">${res.away_team}</h4>
-                                </div>
-                                <div class="col-md-2">
-                                    <p class="fe_pred_2"><strong>Tip</strong></p>
-                                    <p class="fe_pred_2">${res.tip}</p>
-                                </div>
-                                <div class="col-md-2">
-                                    <p class="fe_pred_3"><strong>Date</strong></p>
-                                    <p class="fe_pred_3">${res.date.toString().substring(0,10)}</p>
-                                </div>
-                                </div>`
+                $card = `<tr >
+                            <td><img src="${res.country}" class="img-fluid" /></td>
+                            <td><p class="fe_pred">${res.home_team}</p></td>
+                            <td><p style="color: white">vs</p></td>
+                            <td><p <p class="fe_pred">${res.away_team}</p></td>
+                            <td><p <p class="fe_pred_2">${res.tip}</p></td>
+                         </tr>`
                 
 
                 if(res.page == 1){
-                    $('#admin-data').append($card);
+                    $('#today tbody').append($card);
                 }
 
                 if(res.page == 2){
-                    $('#tomorrow-data').append($card);
+                    $('#tomorrow tbody').append($card);
                 }
                 
             });
