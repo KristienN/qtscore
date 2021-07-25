d44b10e9429888e631ad7ee98eb39e7f0f9e7cbd
KristienN <62649348+KristienN@users.noreply.github.com>
2021-07-25 16:57:54 +0200

    homepage styling

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     let $card;
+    let $row;
     $.ajax({
         type: "GET",
         url: "https://qtscore.herokuapp.com/prediction/",
         success: function (response) {
             response.forEach(res => {
-                $card = `<tr >
+                $card = `<tr>
                             <td><img src="${res.country}" class="img-fluid" /></td>
                             <td><p class="fe_pred">${res.home_team}</p></td>
                             <td><p style="color: white">vs</p></td>
@@ -24,8 +25,25 @@ $(document).ready(function () {
                 
             });
 
-            
-            
+            for (let x = 0; x < 5; x++) {
+                $row = `<tr>
+                            <td><img src="${response[x].country}" class="img-fluid" /></td>
+                            <td><p class="fe_pred">${response[x].home_team}</p></td>
+                            <td><p style="color: white">vs</p></td>
+                            <td><p <p class="fe_pred">${response[x].away_team}</p></td>
+                            <td><p <p class="fe_pred_2">${response[x].tip}</p></td>
+                         </tr>`
+
+                if(response[x].page == 1){
+                    $('.tdf tbody').append($row);
+                }
+
+                if(response[x].page == 2){
+                    $('.tmf tbody').append($row);
+                }
+                
+            }
+      
         }
     });
 });
