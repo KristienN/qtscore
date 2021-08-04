34242b0a5ec8254cc75d4b1f8a6151a8f9949fbb
KristienN <62649348+KristienN@users.noreply.github.com>
2021-08-04 15:23:05 +0200

    table styling fixes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,10 +8,22 @@ $(document).ready(function () {
             response.forEach(res => {
                 $card = `<tr>
                             <td><img src="${res.country}" class="img-fluid" /></td>
-                            <td><p class="fe_pred">${res.home_team}</p></td>
+                            <td>
+                                <div class="fe_pred" style="text-align: center; margin: 0 2rem">
+                                    <p>${res.away_team}</p>
+                                </div>
+                            </td>
                             <td><p style="color: white">vs</p></td>
-                            <td><p <p class="fe_pred">${res.away_team}</p></td>
-                            <td><p <p class="fe_pred_2">${res.tip}</p></td>
+                            <td>
+                                <div class="fe_pred" style="text-align: center; margin: 0 2rem">
+                                    <p>${res.away_team}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="fe_pred_2" style="text-align: center; margin: 0 2rem">
+                                    <p>${res.tip}</p>
+                                </div>
+                            </td>
                          </tr>`
                 
 
@@ -30,8 +42,8 @@ $(document).ready(function () {
                             <td><img src="${response[x].country}" class="img-fluid" /></td>
                             <td><p class="fe_pred">${response[x].home_team}</p></td>
                             <td><p style="color: white">vs</p></td>
-                            <td><p <p class="fe_pred">${response[x].away_team}</p></td>
-                            <td><p <p class="fe_pred_2">${response[x].tip}</p></td>
+                            <td><p class="fe_pred">${response[x].away_team}</p></td>
+                            <td><p class="fe_pred_2">${response[x].tip}</p></td>
                          </tr>`
 
                 if(response[x].page == 1){
